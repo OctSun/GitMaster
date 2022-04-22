4c2bd25146b3c68572eea88ae62591a6f2acb855
Biginner899 <rkarkgkq@naver.com>
2022-04-22 12:25:11 +0900

    test

--- app/src/main/java/kr/co/khs/gitmaster/MainActivity.java
+++ app/src/main/java/kr/co/khs/gitmaster/MainActivity.java
@@ -3,6 +3,14 @@ package kr.co.khs.gitmaster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Base64;
+
+import java.io.FileInputStream;
+import java.security.Key;
+import java.security.KeyStore;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.cert.Certificate;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +19,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         int a = 0;
+//        try {
+//
+//            //FileInputStream is = new FileInputStream("/Users/hosang/GitMaster.jks");
+//            FileInputStream is = new FileInputStream("/Users/hosang/a.txt");
+//            KeyStore keystore = KeyStore.getInstance(KeyStore.getDefaultType());
+//            String password = "123456";
+//            char[] passwd = password.toCharArray();
+//            keystore.load(is, passwd);
+//            String alias = "key0";
+//            Key key = keystore.getKey(alias, passwd);
+//            if (key instanceof PrivateKey) {
+//                // Get certificate of public key
+//                Certificate cert = keystore.getCertificate(alias);
+//                // Get public key
+//                PublicKey publicKey = cert.getPublicKey();
+//
+////                String publicKeyString = Base64.encodeBase64String(publicKey
+////                        .getEncoded());
+//                String publicKeyString  = Base64.encodeToString(publicKey.getEncoded(),0);
+//                System.out.println("Key = "+publicKeyString);
+//
+//            }
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+
     }
+
+
 }
